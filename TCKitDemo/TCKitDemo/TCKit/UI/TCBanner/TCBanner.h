833c4f3587deafe742dcb2994070574fed2fc49f
崔正清 <270310922@qq.com>
2017-06-27 22:05:14 +0800

    修改banner逻辑
    修改banner逻辑

--- TCKitDemo/TCKitDemo/TCKit/UI/TCBanner/TCBanner.h
+++ TCKitDemo/TCKitDemo/TCKit/UI/TCBanner/TCBanner.h
@@ -8,6 +8,30 @@
 
 #import <UIKit/UIKit.h>
 
+/*
+ *  定义了banner的滚动方向
+ *  @param  TCBannerScrollDirectionHorizontal           水平方向从左向右
+ *  @param  TCBannerScrollDirectionHorizontalReverse    水平方向从右向左
+ *  @param  TCBannerScrollDirectionVertical             竖直方向从上往下
+ *  @param  TCBannerScrollDirectionVerticalReverse      竖直方向从下往上
+ */
+typedef NS_ENUM(NSInteger,TCBannerScrollDirection) {
+    TCBannerScrollDirectionHorizontal,
+    TCBannerScrollDirectionHorizontalReverse,
+    TCBannerScrollDirectionVertical,
+    TCBannerScrollDirectionVerticalReverse
+};
+
 @interface TCBanner : UIView <UIScrollViewDelegate>
+
+/*
+ *  初始化方法
+ *  @param  frame       banner的frame属性
+ *  @param  images      banner中要显示的图片数组,其中保存的是图片名
+ *  @param  direction   banner自动滚动的方向
+ */
+- (instancetype)initWithFrame:(CGRect)frame
+                       images:(NSArray *)images
+                    direction:(TCBannerScrollDirection)direction;
 
 @end
--- TCKitDemo/TCKitDemo/TCKit/UI/TCBanner/TCBanner.m
+++ TCKitDemo/TCKitDemo/TCKit/UI/TCBanner/TCBanner.m
@@ -10,66 +10,117 @@
 //定时器
 #import "TCJX_GCDTimerManager.h"
 
-@implementation TCBanner
-{
-    UIScrollView *_scrollView;
-}
+@interface TCBanner ()
 
-- (instancetype)initWithFrame:(CGRect)frame{
+@property (nonatomic,strong) UIScrollView *scrollView;
+
+@property (nonatomic,copy) NSArray *images;
+
+@property (nonatomic,assign) TCBannerScrollDirection direction;
+
+@end
+
+@implementation TCBanner
+
+
+#pragma mark - 初始化方法
+//万能初始化方法
+- (instancetype)initWithFrame:(CGRect)frame images:(NSArray *)images direction:(TCBannerScrollDirection)direction {
     self = [super initWithFrame:frame];
     if (self) {
-        [self setupScrollView];
+        _images = images;
+        _direction = direction;
     }
     return self;
 }
 
+//- (instancetype)initWithFrame:(CGRect)frame{
+//    self = [super initWithFrame:frame];
+//    if (self) {
+//        [self setupScrollView];
+//    }
+//    return self;
+//}
+
 //初始化scroll view
 - (void)setupScrollView {
-    _scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)];
+    self.scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)];
     _scrollView.backgroundColor = [UIColor blueColor];
     [self addSubview:_scrollView];
-    _scrollView.contentSize = CGSizeMake(self.frame.size.width * 5.0, self.frame.size.height);
+    switch (_direction) {
+        case TCBannerScrollDirectionHorizontal:
+        case TCBannerScrollDirectionHorizontalReverse:
+        {
+            _scrollView.contentSize = CGSizeMake(self.frame.size.width * _images.count, self.frame.size.height);
+        }
+            break;
+        case TCBannerScrollDirectionVertical:
+        case TCBannerScrollDirectionVerticalReverse:
+        {
+            _scrollView.contentSize = CGSizeMake(self.frame.size.width, self.frame.size.height  * _images.count);
+        }
+            break;
+    }
     _scrollView.pagingEnabled = YES;
     _scrollView.delegate = self;
     //添加广告图
     [self setupADSImageViewInScrollView:_scrollView];
-    //设置定时器
-    [self startTimer];
 }
 
 //添加广告图
 - (void)setupADSImageViewInScrollView:(UIScrollView *)scrollView {
-    for (int i = 0; i < 5; i++) {
-        UIImageView *imageView = [[UIImageView alloc] init];
-        imageView.frame = CGRectMake(i * self.frame.size.width, 0, self.frame.size.width, self.frame.size.height);
-        [scrollView addSubview:imageView];
-        switch (i) {
-            case 0:
+    if (_images.count > 1) {
+        for (int i = 0; i < _images.count; i++) {
+            UIImageView *imageView = [[UIImageView alloc] init];
+            switch (_direction) {
+                case TCBannerScrollDirectionHorizontal:
+                case TCBannerScrollDirectionHorizontalReverse:
+                {
+                    imageView.frame = CGRectMake(i * _scrollView.frame.size.width, 0, _scrollView.frame.size.width, _scrollView.frame.size.height);
+                }
+                    break;
+                case TCBannerScrollDirectionVertical:
+                case TCBannerScrollDirectionVerticalReverse:
+                {
+                    imageView.frame = CGRectMake(0, i * _scrollView.frame.size.height, _scrollView.frame.size.width, _scrollView.frame.size.height);
+                }
+                    break;
+            }
+            [scrollView addSubview:imageView];
+            NSString *image = _images[i];
+            NSString *firstImage = [_images firstObject];
+            NSString *lastImage = [_images lastObject];
+            if (i == 0) {
+                imageView.image = [UIImage imageNamed:lastImage];
+            } else if (i == _images.count) {
+                imageView.image = [UIImage imageNamed:firstImage];
+            } else {
+                imageView.image = [UIImage imageNamed:image];
+            }
+        }
+        switch (_direction) {
+            case TCBannerScrollDirectionHorizontal:
+            case TCBannerScrollDirectionHorizontalReverse:
             {
-                imageView.backgroundColor = [UIColor colorWithRed:146/255.0 green:39/255.0 blue:143/255.0 alpha:1];
+                [_scrollView scrollRectToVisible:CGRectMake(_scrollView.frame.size.width, 0, _scrollView.frame.size.width, _scrollView.frame.size.height) animated:NO];
             }
                 break;
-            case 1:
+            case TCBannerScrollDirectionVertical:
+            case TCBannerScrollDirectionVerticalReverse:
             {
-                imageView.backgroundColor = [UIColor colorWithRed:241/255.0 green:89/255.0 blue:42/255.0 alpha:1];
-            }
-                break;
-            case 2:
-            {
-                imageView.backgroundColor = [UIColor colorWithRed:248/255.0 green:147/255.0 blue:31/255.0 alpha:1];
-            }
-                break;
-            case 3:
-            {
-                imageView.backgroundColor = [UIColor colorWithRed:140/255.0 green:197/255.0 blue:64/255.0 alpha:1];
-            }
-                break;
-            default:
-            {
-                imageView.backgroundColor = [UIColor colorWithRed:9/255.0 green:179/255.0 blue:205/255.0 alpha:1];
+                [_scrollView scrollRectToVisible:CGRectMake(0, _scrollView.frame.size.height, _scrollView.frame.size.width, _scrollView.frame.size.height) animated:NO];
             }
                 break;
         }
+        //设置定时器
+        [self startTimer];
+    } else if (_images.count == 1) {
+        UIImageView *imageView = [[UIImageView alloc] init];
+        imageView.frame = CGRectMake(0, 0, self.frame.size.width, self.frame.size.height);
+        NSString *image = _images[0];
+        imageView.image = [UIImage imageNamed:image];
+    } else {
+        NSLog(@"%@",[NSString stringWithFormat:@"传入banner的图片数组中没有元素 %s",__func__]);
     }
 }
 
@@ -95,8 +146,33 @@
 //滚动scroll view
 - (void)scrollScrollView {
     //获取当前scroll view显示的index
-    NSInteger index = round(_scrollView.contentOffset.x / _scrollView.frame.size.width);
-    [_scrollView scrollRectToVisible:CGRectMake(_scrollView.frame.size.width * (index + 1), 0, _scrollView.frame.size.width, _scrollView.frame.size.height) animated:YES];
+    switch (_direction) {
+        case TCBannerScrollDirectionHorizontal:
+        {
+            NSInteger index = round(_scrollView.contentOffset.x / _scrollView.frame.size.width);
+            [_scrollView scrollRectToVisible:CGRectMake(_scrollView.frame.size.width * (index + 1), 0, _scrollView.frame.size.width, _scrollView.frame.size.height) animated:YES];
+        }
+            break;
+        case TCBannerScrollDirectionHorizontalReverse:
+        {
+            NSInteger index = round(_scrollView.contentOffset.x / _scrollView.frame.size.width);
+            [_scrollView scrollRectToVisible:CGRectMake(_scrollView.frame.size.width * (index - 1), 0, _scrollView.frame.size.width, _scrollView.frame.size.height) animated:YES];
+        }
+            break;
+        case TCBannerScrollDirectionVertical:
+        {
+            NSInteger index = round(_scrollView.contentOffset.y / _scrollView.frame.size.height);
+            [_scrollView scrollRectToVisible:CGRectMake(0, _scrollView.frame.size.height * (index + 1), _scrollView.frame.size.width, _scrollView.frame.size.height) animated:YES];
+        }
+            break;
+        case TCBannerScrollDirectionVerticalReverse:
+        {
+            NSInteger index = round(_scrollView.contentOffset.y / _scrollView.frame.size.height);
+            [_scrollView scrollRectToVisible:CGRectMake(0, _scrollView.frame.size.height * (index - 1), _scrollView.frame.size.width, _scrollView.frame.size.height) animated:YES];
+        }
+            break;
+    }
+    
 }
 
 //scroll view 代理方法
@@ -104,35 +180,45 @@
     [self stopTimer];
 }
 
-//- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate {
-//    NSLog(@"拖拉结束");
-//    [self startTimer];
-//    [self didScrollEdge:scrollView];
-//}
-
 - (void)scrollViewDidEndScrollingAnimation:(UIScrollView *)scrollView {
-    NSLog(@"动画结束");
     [self didScrollEdge:scrollView];
 }
 
 - (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
-    NSLog(@"减速结束");
     [self startTimer];
     [self didScrollEdge:scrollView];
 }
 
 //滚动到边界时
 - (void)didScrollEdge:(UIScrollView *)scrollView {
-    NSInteger index = round(scrollView.contentOffset.x / scrollView.frame.size.width);
-    if (index == 4) {
-        [scrollView scrollRectToVisible:CGRectMake(0, 0, scrollView.frame.size.width, scrollView.frame.size.height) animated:NO];
+    switch (_direction) {
+        case TCBannerScrollDirectionHorizontal:
+        case TCBannerScrollDirectionHorizontalReverse:
+        {
+            NSInteger index = round(scrollView.contentOffset.x / scrollView.frame.size.width);
+            if (index == _images.count) {
+                [scrollView scrollRectToVisible:CGRectMake(0, 0, scrollView.frame.size.width, scrollView.frame.size.height) animated:NO];
+            }
+            if (index == 0) {
+                [scrollView scrollRectToVisible:CGRectMake((_images.count - 1) * scrollView.frame.size.width, 0, scrollView.frame.size.width, scrollView.frame.size.height) animated:NO];
+            }
+        }
+            break;
+        case TCBannerScrollDirectionVertical:
+        case TCBannerScrollDirectionVerticalReverse:
+        {
+            NSInteger index = round(scrollView.contentOffset.y / scrollView.frame.size.height);
+            if (index == _images.count) {
+                [scrollView scrollRectToVisible:CGRectMake(0, 0, scrollView.frame.size.width, scrollView.frame.size.height) animated:NO];
+            }
+            if (index == 0) {
+                [scrollView scrollRectToVisible:CGRectMake(0, (_images.count - 1) * scrollView.frame.size.height, scrollView.frame.size.width, scrollView.frame.size.height) animated:NO];
+            }
+        }
+            break;
     }
-    if (index == 0) {
-        [scrollView scrollRectToVisible:CGRectMake(3 * scrollView.frame.size.width, 0, scrollView.frame.size.width, scrollView.frame.size.height) animated:NO];
-    }
-}
 
-//滚动到边界时如何处理
+}
 
 - (void)dealloc {
     NSLog(@"Banner已销毁");
